4a3672cafa7887c1e7ac18918dfe6f4783753e57
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2023-10-17 15:36:51 +0900

    jQueryの動作確認

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import $ from 'jquery'
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('.home-title').on('click', () => {
+    window.alert('CLICKED')
+  })
+})
