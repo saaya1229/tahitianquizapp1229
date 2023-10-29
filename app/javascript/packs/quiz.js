46a228cab143affb694d2655e500bc5cbd264c00
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2023-10-29 14:40:36 +0900

    正解の時は正解！と出てから次の問題へ、不正解の時は不正解！と出てから何処かクリックをすると次の問題へ進む

--- app/javascript/packs/quiz.js
+++ app/javascript/packs/quiz.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 import axios from 'axios';
 
 $(document).ready(function() {
+  let canProceed = false; // 初期状態では進行不可
+
   $('.quiz-question li').on('click', function() {
     const choiceId = $(this).data('choice-id');
     $.ajax({
@@ -11,21 +13,42 @@ $(document).ready(function() {
       headers: {
         'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
       },
-      dataType: 'json',  // JSONデータを受け取ることを指定
+      dataType: 'json',
       success: function(response) {
+        alert(response.message);
         if (response.correct) {
-          alert(response.message);  // 正解不正解のメッセージを表示
           // 正解の場合、次の問題を取得して表示
           getNextQuestion();
         } else {
-          alert(response.message);  // 正解不正解のメッセージを表示
-          // 不正解の場合、次の問題を表示せず、次に進むフラグを設定
-          let canProceed = false;
-          $(document).one('click', function() {
-            canProceed = true;
-          });
+          // 不正解の場合、次に進むフラグを設定
+          canProceed = true;
         }
       }
     });
   });
+
+  // 別の場所をクリックしたら次の問題に進む
+  $(document).on('click', function() {
+    if (canProceed) {
+      getNextQuestion();
+      canProceed = false; // フラグをリセット
+    }
+  });
 });
+
+function getNextQuestion() {
+  $.ajax({
+    type: 'GET',
+    url: '/next_question',
+    success: function(nextQuestion) {
+      // 次の問題を表示
+      if (nextQuestion) {
+        $('.quiz-question p').text(nextQuestion.content);
+        // 選択肢を更新する方法を追加
+        updateChoices(nextQuestion.choices);
+      } else {
+        alert('次の問題が見つかりませんでした。');
+      }
+    }
+  });
+}
